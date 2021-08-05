87629f6408f7226a2bc4af630b343f828d818aec
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-08-05 12:03:06 +0530

    Add files via upload

---
+++ Welcome user/src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+
+// var username = prompt("what is your username")
+
+var username = "Shivendra K Jha";
+
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 style={{backgroundColor : "blue"}}>Welcome {username}</h1>
+    </div>
+  );
+}
